d704bcbe19708a764e7d24b2891905ec6116bb58
ekateriniy <ekaterin.yu@gmail.com>
2022-06-07 06:30:37 +0400

    refactor: remove unused fabrics

--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -1,5 +1,5 @@
-FactoryBot.define do
-  factory :answer do
+# FactoryBot.define do
+#   factory :answer do
     
-  end
-end
+#   end
+# end
--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,6 +1,6 @@
-FactoryBot.define do
-  factory :question do
-    title { "MyString" }
-    body { "MyText" }
-  end
-end
+# FactoryBot.define do
+#   factory :question do
+#     title { "MyString" }
+#     body { "MyText" }
+#   end
+# end
