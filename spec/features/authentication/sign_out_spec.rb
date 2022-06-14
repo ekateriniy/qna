9d1a15f7a59bfb58d92fb29c6e6a7ba78431e371
ekateriniy <ekaterin.yu@gmail.com>
2022-06-14 12:26:58 +0800

    test: add signout acceptance test

---
+++ spec/features/authentication/sign_out_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+feature 'User can sign out', %q{
+  As an authenticated user
+  I'd like to be able to sign out
+} do
+  given(:user) { create(:user) }
+  background { sign_in(user) }
+
+  scenario 'Authenticated user tries to sign out' do
+    visit root_path
+    click_on 'Sign out'
+
+    expect(page).to have_content 'Signed out successfully.'
+  end
+end
