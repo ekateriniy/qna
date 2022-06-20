d58c1ad133d97af3fb0ca0dfdb5b5a3ee98cea1b
ekateriniy <ekaterin.yu@gmail.com>
2022-06-20 11:48:19 +0800

    test: fix trying to edit other user`s answer

--- spec/features/answer/edit_spec.rb
+++ spec/features/answer/edit_spec.rb
@@ -8,12 +8,19 @@ feature 'User can edit his answer', %q{
   given!(:user) { create(:user) }
   given!(:question) { create(:question) }
   given!(:answer) { create(:answer, question: question, author: user) }
+  given(:other_user) { create(:user) }
 
   scenario 'Unauthenticated user can not edit the answer' do
     visit question_path(question)
 
     expect(page).to_not have_link 'Edit'
   end
+
+  scenario "Authenticated user tries to edit other user's answer" do
+      sign_in(other_user)
+      visit question_path(question)
+      expect(page).to_not have_link 'Edit'
+    end
 
 
   describe 'Authenticated user', js: true do
@@ -44,11 +51,6 @@ feature 'User can edit his answer', %q{
         expect(page).to have_selector 'textarea'
         expect(page).to have_content "Body can't be blank"
       end
-    end
-
-
-    scenario "tries to edit other user's answer" do
-      expect(page).to_not have_link 'Edit'
     end
   end
 end
