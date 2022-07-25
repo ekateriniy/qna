9b8855388aa711907acf059247de15db0fccf123
ekateriniy <ekaterin.yu@gmail.com>
2022-07-25 09:33:36 +0800

    test: add authentication with github

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,17 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
   it { should have_many(:awards) }
+  it { should have_many(:authorisations).dependent(:destroy) }
+
+  describe '.find_for_oauth' do
+    let!(:user) { create(:user) }
+    let(:auth) { OmniAuth::AuthHash.new(provider: 'github', uid: '123') }
+    let(:service) { double('FindForOauthService')}
+
+    it 'calls FindForOauthService' do
+      expect(FindForOauthService).to receive(:new).with(auth).and_return(service)
+      expect(service).to receive(:call)
+      User.find_for_oauth(auth)
+    end
+  end
 end
